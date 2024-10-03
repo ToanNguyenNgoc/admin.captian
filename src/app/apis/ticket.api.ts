bee41f115c0958ba96083010653fa2930d768d04
Nguyen Toan <ngoctoan06011998>
2024-10-03 09:40:53 +0700

    update

--- src/app/apis/ticket.api.ts
+++ src/app/apis/ticket.api.ts
@@ -5,5 +5,5 @@ export const TicketApi = {
   get: (params: PageRequest) => AxiosConfig().get('/api/tickets', { params }).then(res => res.data),
   getById: (id: number) => AxiosConfig().get(`/api/tickets/${id}`).then(res => res.data),
   post: (body: PostTicket) => AxiosConfig().post('/api/tickets', body).then(res => res.data),
-  update: (id: number, body: PostTicket) => AxiosConfig().put(`/api/tickets/${id}`, body).then(res => res.data)
+  update: (id: number, body: PostTicket) => AxiosConfig().patch(`/api/tickets/${id}`, body).then(res => res.data)
 }
